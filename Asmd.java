615c34ea161ea1560895bd6b871d7b1343512bcb
ravi143pvrs <107831877+ravi143pvrs@users.noreply.github.com>
2022-06-22 13:46:20 +0530

    Add files via upload

---
+++ Asmd.java
@@ -0,0 +1,19 @@
+public class Asmd {
+
+	public static void main(String[] args) {
+		
+	int a = 15;
+	int b = 25;
+	int c = a*b;
+	int d = a+b;
+	int e = b-a;
+	int f = b/a;
+
+		System.out.println("multiplication of numbers:-  "+c);
+		System.out.println("addition of numbers:-  "+d);
+		System.out.println("subtraction of numbers:-  "+e);
+		System.out.println("division of numbers:-  "+f);
+		
+	}
+
+}
