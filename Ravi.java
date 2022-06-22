9a1180e9309ee7271c606ab8253918a2767f7a3f
ravi143pvrs <107831877+ravi143pvrs@users.noreply.github.com>
2022-06-22 21:44:14 +0530

    Add files via upload

---
+++ Ravi.java
@@ -0,0 +1,11 @@
+public class Ravi {
+
+	public static void main(String[] args) {
+		
+		System.out.println("name:- Ravi kumar");
+		System.out.println("course:- java");
+		System.out.println("branch:- btech in EEE");
+	}
+
+
+}
