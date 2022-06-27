6e5449db9163aaf227c9901080573845f5718fb7
ravi143pvrs <107831877+ravi143pvrs@users.noreply.github.com>
2022-06-27 15:17:44 +0530

    Add files via upload

---
+++ Ifelseif.java
@@ -0,0 +1,16 @@
+package week01;
+
+public class Ifelseif {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		int b =6;
+		if(a+b < 10) {
+			System.out.println("a + b is less than 10");
+		} else {
+			System.out.println("a + b is greater than 10");
+		}
+	}
+
+}
