8b2767f4656b93c155987e0f646c53d32c76f9e6
ravi143pvrs <107831877+ravi143pvrs@users.noreply.github.com>
2022-06-27 15:42:10 +0530

    Add files via upload

---
+++ Forloop.java
@@ -0,0 +1,14 @@
+package week01;
+
+public class Forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		for (int j = 0; j <=10; j++) {
+			 i = i + j;
+		}
+		System.out.println("the sum of first 10 natural number is:- " +i);
+	}
+
+}
