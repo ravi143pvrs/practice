890fa819ea480c7a9dbf94663b8385e6ea3f8418
ravi143pvrs <107831877+ravi143pvrs@users.noreply.github.com>
2022-06-27 16:09:59 +0530

    Add files via upload

---
+++ Dowhileloop.java
@@ -0,0 +1,15 @@
+package week01;
+
+public class Dowhileloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1;
+		System.out.println("first 10 odd numbers");
+		do {
+			System.out.println(i);
+			i = i + 2;
+		}while (i < 10);
+	}
+
+}
