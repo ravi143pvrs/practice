f1c50d0678f171f4ff4f0297672fe1e4839733f1
ravi143pvrs <107831877+ravi143pvrs@users.noreply.github.com>
2022-06-27 20:12:57 +0530

    Add files via upload

---
+++ Ifelseladder.java
@@ -0,0 +1,22 @@
+package week01;
+
+public class Ifelseladder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "kavya, ravi";
+		if (name == "sravya") {
+			System.out.println("name is sravya");
+		}
+	 if (name == "chitti") {
+			System.out.println("name is chitti");
+		}
+		else if (name == "ravi") {
+			System.out.println("name is ravi");
+		}
+		else {
+			System.out.println(name);
+		}
+	}
+
+}
