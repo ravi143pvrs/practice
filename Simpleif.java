2aa1e4b1ed6dd68c1a9d0d3d32335315e08bd466
ravi143pvrs <107831877+ravi143pvrs@users.noreply.github.com>
2022-06-27 16:48:09 +0530

    Add files via upload

---
+++ Simpleif.java
@@ -0,0 +1,14 @@
+package week01;
+
+public class Simpleif {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 5;
+		int y = 3;
+		if (x+y < 10) {
+			System.out.println("x + y is less than 10");
+		}
+	}
+
+}
