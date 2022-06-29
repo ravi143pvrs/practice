2a20771d00bde7d3d133e735332cdbdd19604e5f
ravi143pvrs <107831877+ravi143pvrs@users.noreply.github.com>
2022-06-29 13:26:51 +0530

    Add files via upload

---
+++ Areaaofsquare.java
@@ -0,0 +1,28 @@
+package week01;
+
+public class Areaaofsquare {
+
+int length;
+int width;
+void insert(int l) {
+	length = l;
+	
+}
+void calculateArea() {
+	System.out.println(length*length);
+	}
+class Square {
+
+public static void main(String[] args) {
+
+Areaaofsquare r1 = new Areaaofsquare();
+	r1.insert(12);
+	r1.calculateArea();
+
+}
+}
+}
+
+
+
+
